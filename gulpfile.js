847124afce84745c36ce7f459f63b359de6d2a67
seaman <semen.bko@ya.ru>
2015-03-09 01:24:06 +0300

    init

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+	stylus = require('gulp-stylus'),
+	jade = require('gulp-jade'),
+	connect = require('gulp-connect');
+
+gulp.task('connect', function(){
+	connect.server({
+		root: 'dest',
+		port: 3000,
+		livereload: true
+	});
+});
+
+gulp.task('html', function(){
+	gulp.src('./src/jade/index.jade')
+		.pipe(jade({
+			pretty: true
+		}))
+		.pipe(gulp.dest('./dest/'))
+		.pipe(connect.reload());
+});
+
+gulp.task('watch', function(){
+	gulp.watch('./src/jade/**/*.jade', ['html']);
+});
+
+gulp.task('default', ['watch', 'connect']);
